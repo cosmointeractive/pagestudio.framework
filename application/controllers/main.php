61026a5059d692ff328a4fd41b10ed8c5d2bb060
Cosmo Mathieu <cmathieu@cliffsliving.com>
2015-09-03 14:44:28 -0400

    Update - Making the default page pretty

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,13 +1,35 @@
 <?php
+/**
+ * PageStudio
+ *
+ * A web application for managing website content. For use with PHP 5.4+
+ * 
+ * This application is based on the PHP framework, 
+ * PIP http://gilbitron.github.io/PIP/. PIP has been greatly altered to 
+ * work for the purposes of our development team. Additional resources 
+ * and concepts have been borrowed from CodeIgniter,
+ * http://codeigniter.com for further improvement and reliability. 
+ *
+ * @package     PageStudio
+ * @author      Cosmo Mathieu <cosmo@cosmointeractive.co>
+ */
+
+// ------------------------------------------------------------------------
 
 class Main extends Controller {
 	
 	function index()
 	{
+        for($i = 0;$i < 100000; $i++) {
+            
+        }
 		$template = $this->loadView('main_view');
+        $this->benchmark->mark('code_end');
+        $template->set('elapsed_time', $this->benchmark->elapsed_time('code_start', 'code_end'));
 		$template->render();
 	}
     
 }
 
-?>
+/* End of file Main.php */
+/* Location: ./application/controllers/Main.php */
--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+	<p class="footer">Page rendered in <strong><?php echo $elapsed_time;?></strong> seconds.</p>
+</div>
+
 </body>
 </html>
